f99be3b9b4cdf042269c78d42fb4d75ab064171b
ArakakiTakuya <225arataku@gmail.com>
2020-09-10 13:16:06 +0900

    Initial Commit

---
+++ Quiz App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz App
+//
+//  Created by 新垣　匠也 on 2020/09/10.
+//  Copyright © 2020 Taku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
