9ec6d498e1921d2b28d8dd8b7eac671450419c1a
ArakakiTakuya <225arataku@gmail.com>
2020-09-10 13:28:25 +0900

    first commit

--- Quiz App/ViewController.swift
+++ Quiz App/ViewController.swift
@@ -2,18 +2,22 @@
 //  ViewController.swift
 //  Quiz App
 //
-//  Created by 新垣　匠也 on 2020/09/10.
+//  Created by Taku on 2020/09/10.
 //  Copyright © 2020 Taku. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textLabel.text = "Hello World !"
     }
+    
+    
+    
 
 
 }
